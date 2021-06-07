04176d9bd547cff7f5ff357faff71ab735ff709e
ajaysharma12799 <ajaysharma12799@gmail.com>
2021-06-08 00:10:12 +0530

    Added main.dart

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card_ui/screen/homeScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
